e71d689d8ee31b391fab9354f8f9d6827f355277
jinzeng <jinzeng@cent285sp11-JIN-Dtop.(none)>
2011-05-03 12:18:43 -1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quiz::Application.config.secret_token = '75da7d87f03cadc2178e8c974396dba598d73ca50d7e619f3f4dc87905d4db7730ed9ae6d2b8303b5af2153cbf90b86e41328ac33556918a2e4b10ed8191cd04'
